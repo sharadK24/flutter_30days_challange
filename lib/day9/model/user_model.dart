2666c4be7a9306b837d0fc0373ba31036342c82e
Sharad Kharade <sharadkharade24@gmail.com>
2026-01-07 23:01:54 +0530

    Merge remote-tracking branch 'origin/master'

--- lib/day9/model/user_model.dart
+++ lib/day9/model/user_model.dart
@@ -6,30 +6,30 @@ class UserModel {
   final String mobileno;
 
   UserModel({
-    //required this.name,
+    required this.name,
     required this.emailid,
-    //required this.username,
+    required this.username,
     required this.city,
-    //required this.mobileno,
+    required this.mobileno,
   });
 
   factory UserModel.fromJson(Map<String, dynamic> json) {
     return UserModel(
       name: json['name'],
-     // emailid: json['emailid'],
-      //username: json['username'],
-      //city: json['city'],
+      emailid: json['emailid'],
+      username: json['username'],
+      city: json['city'],
       mobileno: json['mobileno'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-     // // "name": name,
-     //  "emailid": emailid,
-     //  "username": username,
-     //  "city": city,
-     //  "mobileno": mobileno,
+      "name": name,
+     "emailid": emailid,
+     "username": username,
+     "city": city,
+     "mobileno": mobileno,
 
     };
   }
